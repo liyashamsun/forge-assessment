7f72eb435f4e978b48712ec41932634a1422d4e7
Liya Saffura Shamsun Anwar <liyaanwar@heitech.com.my>
2022-01-12 00:57:31 +0800

    test for send param

--- src/Body.tsx
+++ src/Body.tsx
@@ -1,16 +1,51 @@
 import React from 'react';
-import ForgeUI, { render, Fragment, Form, TextField, Button, Radio, RadioGroup, useState } from '@forge/ui';
-import { countWordFx } from './CountStr';
+import ForgeUI, { Table, Head, Cell, Row, Fragment, Form, TextField, Text, Radio, RadioGroup, useState } from '@forge/ui';
+
+interface inputData{
+    txtInput: string;
+    chckBoxValue: string;
+    radioValue: string
+}
+
+interface mainTxt{
+    word: string;
+    occurance: number;
+}
 
 export default function Body(){
-    const [formState, setFormState] = useState(undefined);
+    const [formWords, setFormWords] = useState<inputData[]>();
 
     const onSubmit = async (formData) => {
-        alert('here');
-        console.log(formData);
-        countWordFx(formData);
-        setFormState(formData);
+        setFormWords(formData);
+        let result: mainTxt[] = countWordFx(formData.txtInput);
     } 
+    
+    const countWordFx = (str: string) => {
+    
+        let strWords = str.split(" ");
+        let strExist: mainTxt[] = [];
+    
+        for(let i in strWords){
+            let newWord = strWords[i];
+            let newWordArr: mainTxt={
+                word:newWord.toString(),
+                occurance:1
+            };
+            if (strExist.length>0){
+                if(strExist.some(exist => exist.word === newWord)){
+                    let getIndex = strExist.findIndex(exist => exist.word === newWord);
+                    strExist[getIndex].occurance++;
+                }else{
+                    strExist.push(newWordArr);
+                }
+            }else{
+                strExist.push(newWordArr);
+            }
+        }
+    
+        console.log("no error thanks");
+        return(strExist);
+    }
     return(
         <Fragment>
             <Form onSubmit={onSubmit}>
@@ -24,8 +59,18 @@ export default function Body(){
                 <Radio defaultChecked label="Ascending" value="asc" />
                 <Radio label="Descending" value="desc" />
                 </RadioGroup> 
-                
             </Form>
+            <Table>
+                <Head>
+                <Cell>
+                    <Text>Word</Text>
+                </Cell>
+                <Cell>
+                    <Text>Occurance</Text>
+                </Cell>
+                </Head>
+            </Table>
+            {formWords && <Text>{JSON.stringify(formWords)}</Text>}
         </Fragment>
-    );
+
 }
